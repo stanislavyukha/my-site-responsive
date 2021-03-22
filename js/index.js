222915c676aa0806af32e3e0fa459c54705ead4a
Stas Yukha <valhalla245@gmail.com>
2021-03-22 12:45:38 +0200

    test

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 $(function () {
-            $(".before-after-container[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.7});
-           
-        });
+  $(".before-after-container[data-orientation!='vertical']").twentytwenty({ default_offset_pct: 0.7 });
+
+});
 
 const sliderHeight = document.querySelector("#height");
 const sliderWeight = document.querySelector("#weight");
 let currentHeight = document.querySelector("#height-value");
 let currentWeight = document.querySelector("#weight-value");
-currentHeight.innerHTML = "Your height is " + sliderHeight.value + " cm" ; 
+currentHeight.innerHTML = "Your height is " + sliderHeight.value + " cm";
 currentWeight.innerHTML = "Your weight is " + sliderWeight.value + " kg";
 
-sliderHeight.oninput = function() {
+sliderHeight.oninput = function () {
   currentHeight.innerHTML = "Your height is " + this.value + " cm";
 }
-sliderWeight.oninput = function() {
+sliderWeight.oninput = function () {
   currentWeight.innerHTML = "Your weight is " + this.value + " kg";
 }
 let result = document.getElementById("bmi");
@@ -21,8 +21,8 @@ let bmIndex;
 
 document.querySelector('.calculate-bmi').addEventListener('click', calcBMI);
 function calcBMI() {
-  bmIndex = (sliderWeight.value / Math.pow(sliderHeight.value / 100,2)).toFixed(2); 
-  if (bmIndex < 18.5) result.innerHTML = "Your BMI is " + `<strong> ${bmIndex} </strong>`  + ", you are underweight";
+  bmIndex = (sliderWeight.value / Math.pow(sliderHeight.value / 100, 2)).toFixed(2);
+  if (bmIndex < 18.5) result.innerHTML = "Your BMI is " + `<strong> ${bmIndex} </strong>` + ", you are underweight";
   else if (bmIndex < 25) result.innerHTML = "Your BMI is " + `<strong> ${bmIndex} </strong>` + ", your weight is normal";
   else if (bmIndex < 29.9) result.innerHTML = "Your BMI is " + `<strong> ${bmIndex} </strong>` + ", you are overweight";
   else result.innerHTML = "Your BMI is " + `<strong> ${bmIndex} </strong>` + ", you are obese";
@@ -44,10 +44,10 @@ function calcDiet() {
   const diet = document.querySelector('#diet');
   let result;
   for (const radio of radios) {
-      if (radio.checked) {
-          gender = radio.value;
-          break;
-      }
+    if (radio.checked) {
+      gender = radio.value;
+      break;
+    }
   }
   if (age.value == 0 || age.value < 0) {
     alert("please input your age");
@@ -60,7 +60,7 @@ function calcDiet() {
   if (gender === "female") result = ((10 * sliderWeight.value + 6.25 * sliderHeight.value - 5 * age.value - 161) * coefficient).toFixed(0);
   else result = ((10 * sliderWeight.value + 6.25 * sliderHeight.value - 5 * age.value + 5) * coefficient).toFixed(0);
   diet.innerHTML = "Your proper amount of calories is " + `<strong> ${result} </strong>` + " kcal/day";
-  
+
 
 };
 
@@ -74,3 +74,4 @@ function addOrder() {
 
 // Для женщин: (10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) − 161
 // Для мужчин: (10 × вес в килограммах) + (6,25 × рост в сантиметрах) − (5 × возраст в годах) + 5
+
